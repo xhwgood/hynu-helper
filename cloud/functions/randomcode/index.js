6c87133ae035e32311325d25c9c99ad2ddc3bccb
项鸿伟 <734824565@qq.com>
2020-11-10 15:06:51 +0800

    验证码云函数修改注释

--- cloud/functions/randomcode/index.js
+++ cloud/functions/randomcode/index.js
@@ -3,25 +3,25 @@ const axios = require('axios')
 
 // 云函数入口函数
 exports.main = async (e, context) => {
-	let base64
+  let base64
 
-	return await axios(e.url, {
-		responseType: 'arraybuffer'
-	})
-		.then(body => {
-			const getCookie = body.headers['set-cookie']
-			let sessionid = getCookie[0].slice(0, 43)
-			base64 =
-				'data:image/jpg;base64,' + Buffer.from(body.data).toString('base64')
-			// 若有第二个cookie，则是CET查准考证号的验证码，否则就是登录教务处的验证码
-			if (getCookie[1]) {
-				sessionid = sessionid + getCookie[1].slice(0, 42)
-			}
+  return await axios(e.url, {
+    responseType: 'arraybuffer'
+  })
+    .then(body => {
+      const getCookie = body.headers['set-cookie']
+      let sessionid = getCookie[0].slice(0, 43)
+      base64 =
+        'data:image/jpg;base64,' + Buffer.from(body.data).toString('base64')
+      // 若有第二个cookie，则是CET查准考证号的验证码
+      if (getCookie[1]) {
+        sessionid = sessionid + getCookie[1].slice(0, 42)
+      }
 
-			return {
-				base64,
-				sessionid
-			}
-		})
-		.catch(err => console.log('出错了！', err))
+      return {
+        base64,
+        sessionid
+      }
+    })
+    .catch(err => console.log('出错了！', err))
 }
