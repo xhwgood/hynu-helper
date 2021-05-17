b87b717efcabaf731af30d8f1840a3e30ea4c2a3
项鸿伟 <734824565@qq.com>
2021-05-17 19:13:33 +0800

    增加公用列表底部组件

--- client/src/components/list-footer.jsx
+++ client/src/components/list-footer.jsx
@@ -1,6 +1,19 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text, Button } from '@tarojs/components'
+import { View } from '@tarojs/components'
+import { AtIcon } from 'taro-ui'
+
 /** 有列表的页面底部loading */
-const ListFooter = ({ hasNext }) => hasNext ? <View>数据正快马加鞭赶来……</View> : <View>没有更多了~</View>
+const ListFooter = ({ hasNext }) => hasNext ? (
+  <View>
+    <AtIcon
+      value='loading-3'
+      customStyle={{
+        transform: 'rotate(360deg)',
+        transition: 'All 0.8s ease'
+      }}
+    />
+    数据正快马加鞭赶来……
+  </View>
+) : <View>没有更多了~</View>
 
 export default ListFooter
